5612c9da870da00a904ee51367a716d1a6b45cb2
Lilian LEMELIN <l0lemeli@enib.fr>
2025-06-10 16:48:14 +0200

    Added emit signal from SearchComponent. Added listen signal in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="home">
-    <SearchComponent />
+    <SearchComponent @search="search" />
     <ResultatComponent ref="results" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import SearchComponent from '@/components/SearchComponent.vue'
 import ResultatComponent from '@/components/ResultatComponent.vue'
-import { ref } from 'vue'
 
 const results = ref<null | InstanceType<typeof ResultatComponent>>()
 
 function search(author: string) {
   results.value?.searchAuthor(author)
 }
-
-search('')
 </script>
